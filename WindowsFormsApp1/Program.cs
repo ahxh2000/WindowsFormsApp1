b5f0874ba57e49cea0cb52b1278e2bea18e0e0dc
晓华 何 <ahxh2000@163.com>
2019-03-10 22:15:25 +0800

    gfdgsdfg

--- WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/Program.cs
@@ -14,11 +14,13 @@ namespace WindowsFormsApp1
         [STAThread]
         static void Main()
         {
+
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new Form1());
             Application.Run(new Form1());
-
+            fdsasfasdf
             //Application.Run(new Form1())33;
         }
     }
